c636abe8a00fd7b35386858abca64132beb79038
kasiluote <7641018+kasiluote@users.noreply.github.com>
2018-09-14 16:27:59 +0800

    rename...

--- chap4/conftest.py
+++ chap4/conftest.py
@@ -1,5 +1,5 @@
 #Defining your own assertion comparison
-from chap3.test_foocompare import Foo
+from chap4.test_foocompare import Foo
 def pytest_assertrepr_compare(op,left,right):
     if isinstance(left,Foo) and isinstance(right,Foo) and op == "==":
         return ['Comparing Foo instance:',
