71b899a4276923024391d8537ad3fe6461e9a21e
kasiluote <7641018+kasiluote@users.noreply.github.com>
2018-09-14 10:59:38 +0800

    modify conftest.py

--- chap4/conftest.py
+++ chap4/conftest.py
@@ -100,10 +100,18 @@ def ddd(p1,p2=2,p3=3):
 
 @pytest.fixture(scope="module")
 def f_req_module2(request):
-    pp1 = getattr(request.module,"p1",5)
+    pp1 = getattrd(request.module,"p1",5)
     f_req_module2 = ddd(pp1,2,3)
     yield f_req_module2
     print("finalizing %s (%s)" % (f_req_module2, pp1))
     print("teardown ddd")
 
+@pytest.fixture(scope="module",
+                params=["smtp.qq.com","mail.python.org"])
+def smtp_connection(request):
+    import smtplib
+    smtp_connection = smtplib.SMTP(request.param, 587 ,timeout = 10)
+    yield smtp_connection
+    print("finalizing %s" % smtp_connection)
+    smtp_connection.close()
 
